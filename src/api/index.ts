41825eab2a3781840d4d3258676dcf9426c9ef3e
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2019-11-11 13:16:19 -0500

    Move api related classes into separate directory

---
+++ src/api/index.ts
@@ -0,0 +1,24 @@
+/**
+ * The http method options interface
+ */
+export { default as IHttpMethodFactoryOptions } from './http-method-factory-options';
+
+/**
+ * Interface for HTTP request parameters.
+ */
+export { default as IRequestParams } from './request-params.ts';
+
+/**
+ * Template mapping statement options.
+ */
+export { default as IStatementOptions } from './statement-options.ts';
+
+/**
+ * Reference to the HTTP method factory class.
+ */
+export { default as HttpMethodFactory } from './http-method-factory';
+
+/**
+ * Reference to the template builder class.
+ */
+export { default as TemplateBuilder } from './template-builder';
