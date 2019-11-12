20f3225f8d7369bd37ba638af7ed065cfb17f126
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2019-11-12 08:54:05 -0500

    Fix bugs with secret manager

--- src/utils/secret-manager.ts
+++ src/utils/secret-manager.ts
@@ -1,5 +1,5 @@
 import { argValidator as _argValidator } from '@vamship/arg-utils';
-import { KMS } from 'aws-sdk';
+import { config as awsConfig, KMS, SharedIniFileCredentials } from 'aws-sdk';
 import { Promise } from 'bluebird';
 
 /**
@@ -7,13 +7,19 @@ import { Promise } from 'bluebird';
  */
 export default class SecretManager {
     private _keyId: string;
+    private _profile: string;
 
     /**
      * @param keyId The id of the key to use when generating encrypted values.
+     * @param profile The name of the AWS profile to use when invoking the the
+     *        KMS API.
      */
-    constructor(keyId: string) {
-        _argValidator.checkString(keyId, 1, 'Invalid key id (arg #1)');
+    constructor(keyId: string, profile = process.env.AWS_DEFAULT_PROFILE) {
+        _argValidator.checkString(keyId, 1, 'Invalid keyId (arg #1)');
+        _argValidator.checkString(profile, 1, 'Invalid profile (arg #2)');
+
         this._keyId = keyId;
+        this._profile = profile as string;
     }
 
     /**
@@ -24,21 +30,36 @@ export default class SecretManager {
      *
      * @returns A secret manager instance.
      */
-    public static async create(alias: string): Promise<SecretManager> {
+    public static async create(
+        alias: string,
+        profile = process.env.AWS_DEFAULT_PROFILE
+    ): Promise<SecretManager> {
+        _argValidator.checkString(alias, 1, 'Invalid alias (arg #1)');
+        _argValidator.checkString(profile, 1, 'Invalid profile (arg #2)');
+
+        awsConfig.credentials = new SharedIniFileCredentials({ profile });
         const kms = new KMS();
 
         const listAliases = Promise.promisify(kms.listAliases.bind(kms));
 
         const result = await listAliases({});
-        const keyId = result.Aliases.find(
+        const keyInfo = result.Aliases.find(
             ({ AliasName: aliasName }) => aliasName === alias
         );
 
-        if (!keyId) {
+        if (!keyInfo) {
             throw new Error(`Could not find KMS key for alias: [${alias}]`);
         }
 
-        return new SecretManager(keyId);
+        const { TargetKeyId: keyId } = keyInfo;
+
+        if (!keyId) {
+            throw new Error(
+                `Alias is not associated with a target key id: [${alias}]`
+            );
+        }
+
+        return new SecretManager(keyId, profile);
     }
 
     /**
@@ -49,18 +70,23 @@ export default class SecretManager {
      * @returns An encrypted string using the key associated with this class.
      */
     public async encrypt(plaintext: string): Promise<string> {
+        _argValidator.checkString(plaintext, 1, 'Invalid plaintext (arg #1)');
+
+        awsConfig.credentials = new SharedIniFileCredentials({
+            profile: this._profile
+        });
         const kms = new KMS();
 
         const encrypt = Promise.promisify(kms.encrypt.bind(kms));
 
         const result = await encrypt({
             KeyId: this._keyId,
-            PlainText: plaintext
+            Plaintext: plaintext
         });
 
-        if(!result || !result.CipherTextBlob) {
+        if (!result || !result.CiphertextBlob) {
             throw new Error('Error encrypting string using KMS');
         }
-        return result.CipherTextBlob;
+        return result.CiphertextBlob.toString('base64');
     }
 }
