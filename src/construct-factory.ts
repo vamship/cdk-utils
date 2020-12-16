c6dbf5dd0ac9ea7db9af77d38637a5ad8b226a16
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2020-12-16 15:44:09 -0500

    Revert changes to fix errors made in haste

--- src/construct-factory.ts
+++ src/construct-factory.ts
@@ -102,9 +102,7 @@ export default abstract class ConstructFactory<
      *        be bound.
      * @returns An object that wraps the construct
      */
-    private _getConstructInfo(
-        scope: Stack | CfnResource
-    ): ConstructInfo<TConstruct> {
+    private _getConstructInfo(scope: Stack): ConstructInfo<TConstruct> {
         const { stackName } = scope;
 
         let constructInfo = this._constructMap[stackName];
